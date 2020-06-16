971ea84a891718cb19b7541dbe17d58a86aa63ca
jctaoo <jctaoo@outlook.com>
2020-06-17 01:57:06 +0800

    init xcode project

---
+++ YuiEditor/YuiEditor.h
@@ -0,0 +1,19 @@
+//
+//  YuiEditor.h
+//  YuiEditor
+//
+//  Created by jctao on 2020/6/17.
+//  Copyright © 2020 jctaoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YuiEditor.
+FOUNDATION_EXPORT double YuiEditorVersionNumber;
+
+//! Project version string for YuiEditor.
+FOUNDATION_EXPORT const unsigned char YuiEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YuiEditor/PublicHeader.h>
+
+
